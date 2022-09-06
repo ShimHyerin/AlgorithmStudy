587515014999db99c17225a0ffe6bad909726231
ShimHyerin <54926467+ShimHyerin@users.noreply.github.com>
2022-09-06 15:36:01 +0900

    [Silver IV] Title: 보물, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+ans = 0
+for i in range(n):
+  ans += a[i]*b[i]
+
+print(ans)
