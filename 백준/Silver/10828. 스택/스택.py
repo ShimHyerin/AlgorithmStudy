17b4faaac7514a1189241f932281ae75de05ab77
ShimHyerin <54926467+ShimHyerin@users.noreply.github.com>
2023-04-12 16:52:47 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -2,22 +2,39 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-stack = []
 
+def push(stack, x):
+    stack.append(x)
+    return stack
+
+def pop(stack):
+    if stack: print(stack.pop())
+    else: print(-1)
+    return stack
+
+def size(stack):
+    print(len(stack))
+
+def empty(stack):
+    if stack: print(0)
+    else: print(1)
+
+def top(stack):
+    if stack: print(stack[-1])
+    else: print(-1)
+
+stack = []
 for i in range(n):
-    command = input().split()
-    if command[0] == 'push':
-        stack.insert(0, command[1])
-    if command[0] == 'pop':
-        if stack:
-            print(stack[0])
-            stack.pop(0)
-        else: print(-1)
-    if command[0] == 'size':
-        print(len(stack))
-    if command[0] == 'empty':
-        if stack: print(0)
-        else: print(1)
-    if command[0] == 'top':
-        if stack: print(stack[0])
-        else: print(-1)
+    prompt_ = input().split()
+    prompt = prompt_[0]
+
+    if prompt == 'push':
+        push(stack, prompt_[1])
+    elif prompt == 'pop':
+        pop(stack)
+    elif prompt == 'size':
+        size(stack)
+    elif prompt == 'empty':
+        empty(stack)
+    else:
+        top(stack)
