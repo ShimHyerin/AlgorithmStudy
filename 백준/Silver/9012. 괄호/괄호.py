6ff567d4673ea130c3a517e4dac4d170696a81bb
ShimHyerin <54926467+ShimHyerin@users.noreply.github.com>
2023-04-13 20:58:18 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,13 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
+
+def sol():
+    br = input().rstrip() # bracket string
+    stack = []
+
+    for i in br:
+        if i == '(':
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return 'NO'
+    if stack:
+        return 'NO'
+    else:
+        return 'YES'
+
 for i in range(n):
-    s =input().split('()')
-    ss = ''
-    for i in s:
-        ss+= i
-    while len(s)//2:
-        s = ss.split('()')
-        ss = ''
-        for i in s:
-            ss+= i
-    if len(ss) == 0: print('YES')
-    else: print('NO')
+    print(sol())
