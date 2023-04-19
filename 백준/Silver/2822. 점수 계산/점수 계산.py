52a98e8e0edd450b678ce8ca8b935d0ee98a52d5
ShimHyerin <54926467+ShimHyerin@users.noreply.github.com>
2023-04-19 15:44:50 +0900

    [Silver V] Title: 점수 계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.py
+++ 백준/Silver/2822. 점수 계산/점수 계산.py
@@ -2,21 +2,13 @@ import sys
 input = sys.stdin.readline
 
 li = []
-idx = []
-min_ = 151 # init
-for i in range(1, 9):
-    sc = int(input()) # input score
-    if i < 6:
-        li.append(sc)
-        idx.append(i)
-        if sc < min_: min_ = sc
-    else:
-        if sc > min_:
-            li.append(sc)
-            idx.append(i)
-            idx.pop(li.index(min_))
-            li.pop(li.index(min_))
-            min_ = min(li)
+ 
+for i in range(8):
+    li.append(int(input()))
 
-print(sum(li))
-print(*idx)
+tmp = sorted(li, reverse=True)[:5]
+res = []
+for i in tmp:
+    res.append(li.index(i)+1)
+print(sum(tmp))
+print(*sorted(res))
